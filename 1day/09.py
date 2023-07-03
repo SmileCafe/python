e65225a5879a1609beb4694ad6f8b2079506fd09
SmileCafe <hiphopbros@gmail.com>
2023-07-03 16:20:53 +0900

    20230703

--- 1day/09.py
+++ 1day/09.py
@@ -1,2 +1,7 @@
-input(firstName)
-input(LastName)
+firstName = input("What's your First Name?")
+lastName = input("What's your Last Name?")
+print("Your Full Name is", lastName + firstName)
+
+number1 = input("정수1: ")
+number2 = input("정수2: ")
+print(number1 + number2)
